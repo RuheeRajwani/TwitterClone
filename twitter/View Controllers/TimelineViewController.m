4e3fd3f5cb87782503d405352d63b05606705bf1
Ruhee Rajwani <ruheer@fb.com>
2022-06-21 14:41:13 -0700

    Added functional table view that shows first 20 tweets in timeline

--- twitter/View Controllers/TimelineViewController.m
+++ twitter/View Controllers/TimelineViewController.m
@@ -10,8 +10,13 @@
 #import "APIManager.h"
 #import "AppDelegate.h"
 #import "LoginViewController.h"
+#import "TweetCell.h"
+#import "Tweet.h"
+#import "UIImageView+AFNetworking.h"
 
-@interface TimelineViewController ()
+@interface TimelineViewController () <UITableViewDataSource, UITableViewDelegate>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (nonatomic, strong) NSMutableArray *arrayOfTweets;
 
 @end
 
@@ -27,18 +32,21 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
     // Get timeline
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
     [[APIManager shared] getHomeTimelineWithCompletion:^(NSArray *tweets, NSError *error) {
         if (tweets) {
+            self.arrayOfTweets = (NSMutableArray*) tweets;
             NSLog(@"😎😎😎 Successfully loaded home timeline");
-            for (NSDictionary *dictionary in tweets) {
-                NSString *text = dictionary[@"text"];
+            for (Tweet *t in tweets) {
+                NSString *text = t.text;
                 NSLog(@"%@", text);
             }
         } else {
             NSLog(@"😫😫😫 Error getting home timeline: %@", error.localizedDescription);
         }
+        [self.tableView reloadData];
     }];
 }
 
@@ -57,5 +65,33 @@
 }
 */
 
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    TweetCell *cell = [tableView dequeueReusableCellWithIdentifier:@"TweetCell" forIndexPath:indexPath];
+    Tweet *tweet = self.arrayOfTweets[indexPath.row];
+    
+    cell.tweetText.text = tweet.text;
+    cell.likeValue.text = [NSString stringWithFormat:@"%d", tweet.favoriteCount];
+    cell.retweetValue.text = [NSString stringWithFormat:@"%d", tweet.retweetCount];
+    cell.date.text= tweet.createdAtString;
+    cell.username.text= tweet.user.screenName;
+    cell.name.text=  tweet.user.name;
+    
+    
+    NSString *URLString = tweet.user.profilePicture;
+    NSURL *url = [NSURL URLWithString:URLString];
+    [cell.profileImage setImageWithURL:url];
+    
+    cell.profileImage.layer.cornerRadius = cell.profileImage.frame.size.height /2;
+    cell.profileImage.layer.masksToBounds = YES;
+    cell.profileImage.layer.borderWidth = 0;
+    
+    return cell;
+    
+}
+
+- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.arrayOfTweets.count;
+}
 
 @end
---
+++ twitter/Views/TweetCell.h
@@ -0,0 +1,34 @@
+//
+//  TweetCell.h
+//  twitter
+//
+//  Created by Ruhee Rajwani on 6/21/22.
+//  Copyright © 2022 Emerson Malca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Tweet.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TweetCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *name;
+@property (weak, nonatomic) IBOutlet UILabel *username;
+@property (weak, nonatomic) IBOutlet UILabel *date;
+@property (weak, nonatomic) IBOutlet UILabel *tweetText;
+@property (weak, nonatomic) IBOutlet UILabel *replyValue;
+@property (weak, nonatomic) IBOutlet UILabel *retweetValue;
+@property (weak, nonatomic) IBOutlet UILabel *likeValue;
+@property (weak, nonatomic) IBOutlet UIButton *reply;
+@property (weak, nonatomic) IBOutlet UIButton *retweet;
+@property (weak, nonatomic) IBOutlet UIButton *like;
+@property (weak, nonatomic) IBOutlet UIButton *message;
+@property (strong, nonatomic) Tweet *tweet;
+@property (weak, nonatomic) IBOutlet UIImageView *profileImage;
+
+
+
+
+@end
+
+NS_ASSUME_NONNULL_END
